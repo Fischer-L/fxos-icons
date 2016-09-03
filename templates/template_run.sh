d4014fa73b7805bdc4db016287f065e3f9324d5c
Fischer.json <fischer.json@gmail.com>
2016-09-03 17:13:15 +0800

    Make install and run executable file

---
+++ templates/template_run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+echo " "
+echo " "
+echo "Converting svg to font icon"
+
+echo " "
+echo " "
+echo "Execute >>> npm install"
+npm install
+
+echo " "
+echo " "
+echo "Execute >>> grunt"
+grunt
+
+printf "\e[32m" # Set green color
+echo " "
+echo " "
+echo "Converting finished... "
+echo "The font icons are saved in the fonts folder."
+echo "Please open index.html to inpsect the font icons and close the termianl window"
+echo -e "\033[0m" # Reset color
